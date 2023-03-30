215222222f36aeba67ac1276380ddc17be1b3295
Coder-Stark <shivamkumar789321@gmail.com>
2023-03-31 00:20:50 +0530

    for deployment changes

--- backend/db.js
+++ backend/db.js
@@ -4,7 +4,6 @@ const dotenv = require('dotenv');
 dotenv.config()
 
 const connect =()=>{
-    //.env=> MONGO = mongodb+srv://shivamkumar:nobitalovesizuka@cluster0.ailmmey.mongodb.net/inotebook?retryWrites=true&w=majority
     mongoose.connect(process.env.MONGO).then(()=>{
         console.log("Connected to DB");
     })
